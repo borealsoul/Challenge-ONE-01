d3fb6ebeb062d94f483855f90ea5f85101295293
Aurora J. Moreira <aurora.jmoreira@gmail.com>
2024-01-27 17:49:11 -0300

    Codificador

--- assets/decodificador.js
+++ assets/decodificador.js
@@ -1,27 +1,50 @@
+const eLetraMinusculaOuEspaco = (string) => /^[a-z]| *$/.test(string);
+// Baseado na resposta:
+// https://stackoverflow.com/questions/1027224/
 
-window.onload = function () {
- }
+function pegarCampoTexto() {
+  return document.getElementById('entrada-texto').value;
+}
+
+function alterarResposta(resposta) {
+  document.getElementById('texto-resposta').innerHTML = resposta;
+}
+
+function eTextoPermitido(texto) {
+  if (texto != '') {
+    for (let i = 0; i < texto.length; i++) {
+      if (!eLetraMinusculaOuEspaco(texto[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
 
 function esconderTelaPadrao() {
   let tela_nao_encontrado = document.getElementById('nao-encontrado');
   let tela_resposta_texto = document.getElementById('resposta');
-  let tela_resposta = document.getElementById('resposta-texto');
 
   tela_nao_encontrado.style.display = 'none';
   tela_resposta_texto.style.display = 'flex';
 
 }
 
-function pegarCampoTexto() {
-  return document.getElementById('entrada-texto').value;
-}
-
 function criptografar() {
   let campo_texto = pegarCampoTexto();
- 
-  if (campo_texto != '') {
-    esconderTelaPadrao();
 
-    document.getElementById('texto-resposta').innerHTML = campo_texto;
+  if (!eTextoPermitido(campo_texto)) {
+    console.log(eTextoPermitido(campo_texto));
+  } else {
+    campo_texto = campo_texto.replaceAll("e", "enter");
+    campo_texto = campo_texto.replaceAll("i", "imes");
+    campo_texto = campo_texto.replaceAll("a", "ai");
+    campo_texto = campo_texto.replaceAll("o", "ober");
+    campo_texto = campo_texto.replaceAll("u", "ufat");
+
+    esconderTelaPadrao();
+    alterarResposta(campo_texto);
   }
 }
+
